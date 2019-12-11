8d4fb4e90720bb2b0abe68ac2cbe664cda4f6b71
KIM HONGIL <clroyan01@gmail.com>
2019-12-12 00:12:05 +0900

    Update redis configuration - redis template

--- src/main/java/boot/lettuce/redis/config/RedisConfiguration.java
+++ src/main/java/boot/lettuce/redis/config/RedisConfiguration.java
@@ -1,10 +1,14 @@
 package boot.lettuce.redis.config;
 
+import boot.lettuce.redis.domain.Hero;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @AllArgsConstructor
@@ -15,5 +19,14 @@ public class RedisConfiguration {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisProperties.getRedisHost(), redisProperties.getRedisPort());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Hero.class));
+        return redisTemplate;
     }
 }
