c28978c48c263660e5b094a664c2eacc2e70d3d1
KIM HONGIL <clroyan01@gmail.com>
2019-12-12 00:12:30 +0900

    Create redis configuration test - redis server

---
+++ src/test/java/boot/lettuce/redis/config/RedisConfigurationTest.java
@@ -0,0 +1,27 @@
+package boot.lettuce.redis.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
+public class RedisConfigurationTest {
+
+    private final RedisServer redisServer;
+
+    public RedisConfigurationTest(final RedisProperties redisProperties) {
+        this.redisServer = new RedisServer(redisProperties.getRedisPort());
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+}
